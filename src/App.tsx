d93a5c6d2144b00d82eaca34cb74bdd130b9cb2e
red-tt-bear <red.tt.bear@gmail.com>
2021-07-09 20:20:35 -0400

    Initial data and integration
    Initial integration with resume template component library
    Initial data (needs tons of revisions)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Website } from '@z87/resume-template';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>This website is still under heavy construction!</p>
+      <Website />
+    </>
   );
 }
 
