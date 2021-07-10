29d4c3addbd5f0a93c7ba54e1fd58c3a441b95af
red-tt-bear <red.tt.bear@gmail.com>
2021-07-09 22:26:30 -0400

    Added linting and fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Website } from '@z87/resume-template';
 
 function App() {
-  return (
-    <>
-      <p>This website is still under heavy construction!</p>
-      <Website />
-    </>
-  );
+    return (
+        <>
+            <p>This website is still under heavy construction!</p>
+            <Website />
+        </>
+    );
 }
 
 export default App;
