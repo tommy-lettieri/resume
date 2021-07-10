d0739de7eaa17b4fb7444e6e21133526c938609b
red-tt-bear <red.tt.bear@gmail.com>
2021-07-10 12:51:38 -0400

    Added qr code to public folder after build

--- utility-scripts/postbuild.js
+++ utility-scripts/postbuild.js
@@ -2,6 +2,10 @@
 /* eslint-disable no-undef */
 
 const fs = require('fs');
-const { version } = require('../package.json');
+const { version, homepage } = require('../package.json');
+const https = require('https');
 
 fs.writeFileSync('./build/version.txt', version);
+const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${homepage}`;
+console.log(`Fetching QR Code: ${qrCodeUrl}`);
+https.get(qrCodeUrl, response => response.pipe(fs.createWriteStream('./build/qrcode.png')));
