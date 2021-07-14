e2b76fe353e9e1d8101f81d13b3c90e642ac8a86
red-tt-bear <red.tt.bear@gmail.com>
2021-07-14 16:00:57 -0400

    Added build time to post build

--- utility-scripts/postbuild.js
+++ utility-scripts/postbuild.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 const { version, homepage } = require('../package.json');
 const https = require('https');
 
+fs.writeFileSync('./build/buildTime.txt', new Date().toString());
 fs.writeFileSync('./build/version.txt', version);
+
 const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${homepage}`;
 console.log(`Fetching QR Code: ${qrCodeUrl}`);
 https.get(qrCodeUrl, response => response.pipe(fs.createWriteStream('./build/qrcode.png')));
