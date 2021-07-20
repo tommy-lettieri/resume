50a7ebeb36c6798d4aec3be9821ddc1fc64d00a5
red-tt-bear <red.tt.bear@gmail.com>
2021-07-20 16:05:17 -0400

    Added consistency by using env variable for version.txt

--- utility-scripts/postbuild.js
+++ utility-scripts/postbuild.js
@@ -6,7 +6,7 @@ const { version, homepage } = require('../package.json');
 const https = require('https');
 
 fs.writeFileSync('./build/buildTime.txt', new Date().toString());
-fs.writeFileSync('./build/version.txt', version);
+fs.writeFileSync('./build/version.txt', process.env.REACT_APP_RESUME_VERSION || version);
 
 const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${homepage}`;
 console.log(`Fetching QR Code: ${qrCodeUrl}`);
